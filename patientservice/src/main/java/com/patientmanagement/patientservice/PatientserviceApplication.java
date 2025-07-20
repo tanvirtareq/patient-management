94c8c023e030ad9eb4939028c945cb3e5c1e9789
tanvirtareq <cse.tanvir.17@gmail.com>
2025-07-20 20:38:36 +0600

    Add Patient Model

---
+++ patientservice/src/main/java/com/patientmanagement/patientservice/PatientserviceApplication.java
@@ -0,0 +1,13 @@
+package com.patientmanagement.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientserviceApplication.class, args);
+	}
+
+}
